8e362e67e06d60a94ca9ccee30aea8403660bc9d
Mark Boszko <mboszko@mac.com>
2019-05-14 19:27:52 -0700

    Adding a progress bar, supposedly.

--- flop_3d_layers.py
+++ flop_3d_layers.py
@@ -4,6 +4,7 @@ import os
 import tempfile
 import zipfile
 from PIL import Image
+from progressbar import ProgressBar
 
 # Logging Setup
 logger = logging.getLogger()
@@ -11,14 +12,14 @@ handler = logging.StreamHandler()
 formatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.ERROR)
 
 
 # Outline of what we need to do:
 # Take command line input of the name of the zip file with images
 def flop_3d_layers(file_path):
     with tempfile.TemporaryDirectory() as temp_path:
-        logger.debug('Created temporary directory: ' + temp_path)
+        logger.debug("Created temporary directory: " + temp_path)
         # Extract the original file.
         zip_ref = zipfile.ZipFile(file_path)
         zip_ref.extractall(temp_path)
@@ -26,15 +27,18 @@ def flop_3d_layers(file_path):
         walk_and_process(temp_path)
         zip_and_save(temp_path, file_path)
 
+
 def walk_and_process(directory):
     """Walk all language subfolders, and rename the files
     with the language code and the hardware / app information
     :param directory: str
     :return:
     """
+    print("Processing...")
+    pbar = ProgressBar()
     for folderName, subfolders, filenames in os.walk(directory):
         logger.debug("CURRENT FOLDER " + folderName)
-        for filename in filenames:
+        for filename in pbar(filenames):
             logger.debug("FILE INSIDE    " + folderName + ": " + filename)
             name, extension = os.path.splitext(filename)
             if extension == ".png":
@@ -42,6 +46,7 @@ def walk_and_process(directory):
                 flop_image(image_path)
             else:
                 logger.debug(">>> Processing OTHER: " + filename)
+
 
 def flop_image(image_path):
     """
@@ -55,6 +60,7 @@ def flop_image(image_path):
     flop_image.save(image_path)
     # flop_image.show()
 
+
 def zip_and_save(temp_path, file_path):
     """
     Re-zip the files with the original name + "_flop"
@@ -64,7 +70,7 @@ def zip_and_save(temp_path, file_path):
     """
     filename, file_extension = os.path.splitext(file_path)
     new_zip_filename = filename + "_flop" + file_extension
-    zip_file = zipfile.ZipFile(new_zip_filename, 'w', zipfile.ZIP_DEFLATED)
+    zip_file = zipfile.ZipFile(new_zip_filename, "w", zipfile.ZIP_DEFLATED)
     for root, dirs, files in os.walk(temp_path):
         for file in files:
             zip_file.write(os.path.join(root, file))
@@ -95,4 +101,3 @@ if __name__ == "__main__":
     if file_extension == ".sl1" or file_extension == ".zip" or force == True:
         logger.debug("Valid file to process.")
         flop_3d_layers(file_path)
-
